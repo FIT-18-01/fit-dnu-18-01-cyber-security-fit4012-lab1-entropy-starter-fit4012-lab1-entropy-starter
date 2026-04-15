f4f75954fc4bbde77f3a5a317dde312c50c9432b
duytuantran2909 <duytuantran2909@gmail.com>
2026-04-15 10:31:33 +0700

    Update mod_inverse.cpp

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -26,19 +26,67 @@ int extended_euclid(int a, int b, int &x, int &y) {
 }
 
 int mod_inverse(int a, int m) {
-    
     int x, y;
-    // Gọi hàm extended_euclid để tìm x và y sao cho: a*x + m*y = gcd(a, m)
     int g = extended_euclid(a, m, x, y);
-
-    // Nghịch đảo modulo chỉ tồn tại khi gcd(a, m) = 1
+    
+    int result = (x % m + m) % m;
     if (g != 1) {
-        return -1;
+        result = -1;
+    }
+    return result;
+}
+
+int main() {
+    int a = 0, m = 0;
+    cout << "Nhap a, m: ";
+    cin >> a >> m;
+
+    if (gcd(a, m) != 1) {
+        cout << "Khong ton tai nghich dao modulo vi gcd(a, m) != 1.\n";
+        return 0;
     }
 
-    // Kết quả x từ thuật toán Euclid mở rộng có thể âm
-    // Ta sử dụng công thức (x % m + m) % m để đưa x về giá trị dương trong khoảng [0, m-1]
-    return (x % m + m) % m;
+    int inv = mod_inverse(a, m);
+    cout << "Nghich dao cua " << a << " mod " << m << " la: " << inv << '\n';
+    cout << "Kiem tra: " << a << " * " << inv << " % " << m
+         << " = " << (1LL * a * inv % m) << '\n';
+    return 0;
+}#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int extended_euclid(int a, int b, int &x, int &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+
+    int x1 = 0, y1 = 0;
+    int g = extended_euclid(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - (a / b) * y1;
+    return g;
+}
+
+int mod_inverse(int a, int m) {
+    int x, y;
+    int g = extended_euclid(a, m, x, y);
+    
+    int result = (x % m + m) % m;
+    if (g != 1) {
+        result = -1;
+    }
+    return result;
 }
 
 int main() {
